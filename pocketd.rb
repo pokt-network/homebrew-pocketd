abad236a62abc042b9b140f70029917390a6850c
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-03-27 20:09:05 -0700

    Update to 0.1.0

--- pocketd.rb
+++ pocketd.rb
@@ -2,7 +2,7 @@ class Pocketd < Formula
   desc "Pocket - A CLI tool for managing your Pocket Network nodes"
   homepage "https://github.com/pokt-network/poktroll"
   url "https://github.com/pokt-network/poktroll/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "4c73bcfe41214a9285c5a5396020552ca777eb6b9defdba0f06b9e39fdf89b7a"
+  sha256 "b9479e20c3e2e125e19ffa48ecf82f0ddf9f1f6e3e77a3326d557d591aa0ca1e"
   license "MIT"
 
   livecheck do
@@ -13,24 +13,24 @@ class Pocketd < Formula
   on_macos do
     on_intel do  # on_macos_do_not_remove
       url "https://github.com/pokt-network/poktroll/releases/download/v0.1.0/pocket_darwin_amd64.tar.gz"
-      sha256 "ba13ffb505b01b6edd59594ab68eff2fb33f42d191635b7750743b072e236f7c"
+      sha256 "ac0112e43ca4d37da7fb389bebd3d77e894530302752007aec92b82a0a9d5119"
     end
 
     on_arm do # on_macos_do_not_remove
       url "https://github.com/pokt-network/poktroll/releases/download/v0.1.0/pocket_darwin_arm64.tar.gz"
-      sha256 "24b9d8d481f1ee2b992f4a9ab9483717b59e599bd3f38150b8d8ad42d7c7d8aa"
+      sha256 "0ad67a7696976adfc893bd50714926d4655680104dfc0bc9190d9eab4fc80ba6"
     end
   end
 
   on_linux do
     on_intel do # on_linux_do_not_remove
       url "https://github.com/pokt-network/poktroll/releases/download/v0.1.0/pocket_linux_amd64.tar.gz"
-      sha256 "82497691ae70d95544834714ed35b09bc5e2f404c2b1541eef69249be37e70fb"
+      sha256 "7baa1309d44cfc4021d01ec11dae1dd67741126243106cc49b87e63778b8bbb6"
     end
 
     on_arm do # on_linux_do_not_remove
       url "https://github.com/pokt-network/poktroll/releases/download/v0.1.0/pocket_linux_arm64.tar.gz"
-      sha256 "4bff80a3441fc2540d89e02ae538a596d22980c184f97a38d4686049380fab45"
+      sha256 "47c3cb4671b499242648c737b7b5bcdaef7feb7b1ccf79f91c2060abd0803d2e"
     end
   end
 
